2b72ba6c11b38ffab460b118eec27b071ff7d829
q02144235 <q02144235@163.com>
2026-07-22 12:44:34 +0800

    方案B:纯Java版EntryHook,替换xposed_init和xposedclass指向Java入口

---
+++ app/src/main/java/com/example/hypernotice/EntryHook.java
@@ -0,0 +1,13 @@
+package com.example.hypernotice;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class EntryHook implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!"com.android.systemui".equals(lpparam.packageName)) return;
+        XposedBridge.log("[HyperNotice] Java入口: handleLoadPackage called");
+    }
+}
