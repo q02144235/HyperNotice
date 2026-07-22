2df741aef39c6790e7058cdb2b32aba70dda314f
q02144235 <q02144235@163.com>
2026-07-22 13:37:06 +0800

    删libxposed:service依赖(需要compileSdk37,我们只有34)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,5 +53,4 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.foundation:foundation")
     compileOnly("io.github.libxposed:api:102.0.0")
-    implementation("io.github.libxposed:service:102.0.0")
 }
